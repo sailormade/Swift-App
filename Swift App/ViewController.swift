d6ce539fa24e79b4de8a4d8f5912444ab7ce77eb
Kenneth Fransson <kenneth.fransson@mac.com>
2016-09-15 15:56:30 +0200

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        theLabel.text = "Hello There!"
+        theLabel.font = UIFont.systemFont(ofSize: 17)
+        tapCount += 1
+        
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times!"
+            tapCount = 0
+        }
+    }
+    
+    @IBAction func newButtonTapped(_ sender: AnyObject) {
+        theLabel.text = "Buttons are cool!"
+        theLabel.font = UIFont.boldSystemFont(ofSize: 17)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
